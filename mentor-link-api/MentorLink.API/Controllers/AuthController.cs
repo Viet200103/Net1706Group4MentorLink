445735f00097d59de438aa04ecc125ec4a7d5e62
VietTL <viettl200103@gmail.com>
2025-03-28 03:40:12 +0700

    Setup config for jwt

---
+++ mentor-link-api/MentorLink.API/Controllers/AuthController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MentorLink.API.Controllers;
+
+public class AuthController: ControllerBase
+{
+    
+}
--- mentor-link-api/MentorLink.API/Program.cs
+++ mentor-link-api/MentorLink.API/Program.cs
@@ -1,11 +1,57 @@
+using System.Text;
+using MentorLink.API.Security;
+using MentorLink.API.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// OAuth Configuration
+var jwtSection = builder.Configuration.GetSection("JwtOptions");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    var jwtOptions = jwtSection.Get<JwtOptions>() ?? throw new Exception($"Missing <{nameof(JwtOptions)}> section.");
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = jwtOptions.ValidAudience,
+        ValidIssuer = jwtOptions.ValidIssuer,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+        ClockSkew = TimeSpan.Zero
+    };
+    
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            string? authCookie = context.Request.Cookies[Contants.AccessToken];
+            if (!authCookie.IsNullOrEmpty())
+            {
+                context.Token = authCookie;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -14,6 +60,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    app.UseCors(policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+    );
 }
 
 app.UseHttpsRedirection();
